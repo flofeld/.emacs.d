718fbaf61d46646ffef209d53f46607204c7b543
Florian Feldmann <flofeld@gmail.com>
2020-01-07 00:38:48 +0100

    added .gitignore

--- configuration.el
+++ configuration.el
@@ -1,3 +1,8 @@
+(local-set-key (kbd "TAB") 'tab-to-tab-stop)
+(setq indent-tabs-mode t)
+(setq tab-width 4)
+(setq backward-delete-char-untabify-method 'hungry)
+
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
@@ -5,6 +10,12 @@
   :config (auto-compile-on-load-mode))
 
 (setq load-prefer-newer t)
+
+(use-package auto-package-update
+:config
+(setq auto-package-update-delete-old-versions t)
+(setq auto-package-update-hide-results t)
+(auto-package-update-maybe))
 
 (load-file "~/.emacs.d/sensible-defaults.el/sensible-defaults.el")
 (sensible-defaults/use-all-settings)
@@ -130,7 +141,7 @@
 
 (global-set-key (kbd "M-/") 'company-complete-common)
 
-(use-package flycheck)
+(use-package magit)
 
 (setq-default tab-width 4)
 
@@ -138,6 +149,100 @@
   :config (global-subword-mode 1))
 
 (setq compilation-scroll-output t)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-follow-delay             0.2
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-desc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package lsp-mode)
+(use-package hydra)
+(use-package company-lsp)
+(use-package lsp-ui)
+(use-package lsp-java :after (lsp)
+  :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+  :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+(require 'dap-java)
+
+(use-package lsp-treemacs)
 
 (use-package css-mode
   :config
@@ -152,6 +257,7 @@
 (use-package go-mode)
 (use-package go-errcheck)
 (use-package company-go)
+(add-to-list 'company-backends 'company-go)
 
 (setenv "GOPATH" "/home/flofeld/go")
 (hrs/append-to-path (concat (getenv "GOPATH") "/bin"))
@@ -161,13 +267,12 @@
 
 (add-hook 'go-mode-hook
           (lambda ()
-            (set (make-local-variable 'company-backends)
-                 '(company-go))
+            (set
             (company-mode)
             (if (not (string-match "go" compile-command))
                 (set (make-local-variable 'compile-command)
                      "go build -v && go test -v && go vet"))
-            (flycheck-mode)))
+            (flycheck-mode))))
 
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
@@ -191,8 +296,24 @@
 
 (add-hook 'go-mode-hook
       (lambda ()
-        (set (make-local-variable 'company-backends) '(company-go))
         (company-mode)))
+
+;  (use-package company-rtags
+;	:config (add-to-list 'company-backends 'company-rtags))
+  (use-package company-c-headers
+	:config (add-to-list 'company-backends 'company-c-headers))
+;  (use-package flycheck-rtags)
+  (use-package irony)
+  (use-package flycheck-irony)
+  (eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+  (use-package company-irony
+	:config (add-to-list 'company-backends 'company-irony))
+
+  (add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 (use-package coffee-mode)
 
@@ -228,6 +349,9 @@
 (eval-after-load 'flycheck
   '(flycheck-package-setup))
 
+(require 'org-agenda)
+(global-set-key "\C-ca" 'org-agenda)
+
 (use-package org)
 
 (use-package org-bullets
@@ -252,16 +376,19 @@
 (use-package htmlize)
 (setq org-html-postamble nil)
 
+;(load "auctex.el" nil t t)
+;(load "preview-latex.el" nil t t)
+
 (customize-set-value 'org-latex-with-hyperref nil)
 
 
-;(require 'ox-latex)
-;(unless (boundp 'org-latex-classes)
-;  (setq org-latex-classes nil))
-;(add-to-list 'org-latex-classes
-;             '("article"
-;               "\\documentclass{article}"
-;               ("\\section{%s}" . "\\section*{%s}")))
+(require 'ox-latex)
+(unless (boundp 'org-latex-classes)
+  (setq org-latex-classes nil))
+(add-to-list 'org-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")))
 
 (setq org-latex-pdf-process
       '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
@@ -308,3 +435,10 @@
 (use-package smartparens)
 (require 'smartparens-config)
 (smartparens-global-mode t)
+
+(use-package yasnippet)
+(use-package yasnippet-snippets)
+(yas-minor-mode t)
+
+(use-package flycheck)
+(global-flycheck-mode t)
